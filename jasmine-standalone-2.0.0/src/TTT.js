50b8cf1a9cb18db0c0182e3d2e6dd90834a70e3f
zhyliana <zhyliana@gmail.com>
2014-06-10 10:48:14 -0700

    adds spec files

---
+++ jasmine-standalone-2.0.0/src/TTT.js
@@ -0,0 +1,13 @@
+(function(root){
+  
+  var TTT = root.TTT = (root.TTT || {});
+  
+  var Game = TTT.Game = function(){
+    this.playerO = "O";
+    this.playerX = "X";
+    this.board = this.makeBoard();
+  }
+  
+  
+  
+})(this);
