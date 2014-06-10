d39b669fcba012e9b088bee5c87cf716b2dae4e7
zhyliana <zhyliana@gmail.com>
2014-06-10 11:35:08 -0700

    fixed diagonalWin. Adds horizontalWin

--- src/TTT.js
+++ src/TTT.js
@@ -3,43 +3,65 @@
   var TTT = root.TTT = (root.TTT || {});
   
   var Game = TTT.Game = function(){
-    this.playerO = "O";
-    this.playerX = "X";
+    this.playerO = Game.marks[0];
+    this.playerX = Game.marks[1];
     this.board = this.makeBoard();
   };
-  
+  Game.marks = ["O", "X"];
   Game.prototype.makeBoard() = function(){
     return _.times(3, function(i)) {
       return _.times(3, function(j)){
         return null
       }
     }
-  }
+  };
   
   Game.prototype.diagonalWin = function(){
     var game = this;
     var diagonal1 = [[0, 0], [1, 1], [2, 2]];
     var diagonal2 = [[2, 0], [1, 1], [0, 2]];
-    var winner = null
+    var winner = null;
     
     _(Game.marks).each(function (mark){
-      var validIdx = _.range(0,3);
-      
-      var won = _(validIdx).any(function(i){
-        return _(validIdx).every(function(j){
-          return game.board[i][j] == mark
-        })
-      })
+      function diagonalWinTest (diagonals){
+        return _.every(positions, function(pos){
+          return game.board[pos[0]][pos[1]] == mark;
+        });
+      }
+      var won = _.any(
+        [diagonal1, diagonal2]
+        diagonalWinTest
+      );
       
       if (won) {
         winner = mark;
       }
-    })
+    });
     
-
-  
-  return winner;
-    
+    return winner;    
   };
+  
+  Game.prototype.horizontalWin = function(){
+    var game = this;
+    var winner = null;
+    _(Game.marks).each(function (mark){
+      var validIdx = _.range(0,3);
+    
+      var won = _(validIdx).any(function(i){
+        return _(validIdx).every(function(j){
+          return game.board[i][j] == mark
+        })
+      });
+      
+      if (won) {
+        winner = mark;
+      }
+      
+    });
+    
+    return winner
+  },
+  
+  
   
 })(this);
